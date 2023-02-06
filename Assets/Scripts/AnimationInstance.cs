d691fc91148e76ce403da52bcc93e32b00ff19ba
NayYan0802 <wenlongnayyan@gmail.com>
2023-02-06 13:49:35 -0500

    Animation List

--- Assets/Scripts/AnimationInstance.cs
+++ Assets/Scripts/AnimationInstance.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class AnimationInstance : MonoBehaviour
 {
-    SSAnimation animationIns;
+    private SSAnimation animationIns;
+
     [SerializeField] private Transform StartPos;
     [SerializeField] private Transform EndPos;
-
     [SerializeField] private float timeDuration;
+
+    [SerializeField] private Transform[] posArray;
+
+    [SerializeField] private float[] timeArray;
+    [SerializeField] private int AniIdx;
 
     private Vector2 CurrentPos;
     private float timeSum;
@@ -18,19 +23,28 @@ public class AnimationInstance : MonoBehaviour
 
     private void Start()
     {
+        AniIdx = 0;
         StartplaySub = EventBus.Subscribe<StartPlay>(_StartPlay);
         timeSum = 0;
         Init();
+
+        //Check Input Validation
+        if (posArray.Length != timeArray.Length + 1)
+        {
+            Debug.LogError("Error pos/time array imput");
+        }
     }
 
     private void Init()
     {
-        animationIns = new SSAnimation(StartPos.position, EndPos.position, timeDuration);
+        //animationIns = new SSAnimation(StartPos, EndPos, timeDuration, this.gameObject);
+        animationIns = new SSAnimation(posArray, timeArray, this.gameObject);
     }
 
     void _StartPlay(StartPlay e)
     {
-        animationIns.StartAnimation(this.gameObject);
+        //animationIns.StartAnimation();
+        StartCoroutine(animationIns.LinearAnimation());
     }
 
     private void OnDestroy()
